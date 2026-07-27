f35bdc6e2d8a60f974926f4e112fa3a4c5378a68
Ilya <dd_business@mail.ru>
2026-07-27 15:25:44 +0300

    Added file selection and statistics page

--- app/routers/calculate_router.py
+++ app/routers/calculate_router.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, Form
+from fastapi import APIRouter, Form, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-from fastapi import Request
 
+from app.database import SessionLocal
+from app.models.file import DownloadedFile
 from app.services.statistics_service import StatisticsService
+
 
 router = APIRouter()
 
@@ -13,18 +15,39 @@ templates = Jinja2Templates(directory="templates")
 @router.post("/calculate", response_class=HTMLResponse)
 def calculate(
         request: Request,
-        files: list[str] = Form(...)
+        files: list[str] = Form([]),
+        all_files: str = Form("false")
 ):
 
-    service = StatisticsService()
+    db = SessionLocal()
 
-    result = service.calculate(files)
+    try:
 
-    return templates.TemplateResponse(
-        request=request,
-        name="statistics.html",
-        context={
-            "request": request,
-            "statistics": result
-        }
-    )
+        if all_files == "true":
+
+            files = [
+                file.filename
+                for file in db.query(
+                    DownloadedFile
+                ).all()
+            ]
+
+
+        service = StatisticsService()
+
+        result = service.calculate(files)
+
+
+        return templates.TemplateResponse(
+            request=request,
+            name="statistics.html",
+            context={
+                "request": request,
+                "statistics": result
+            }
+        )
+
+
+    finally:
+
+        db.close()
